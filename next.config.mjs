356a6cbb3bb83fc16dc09b02457329d6de727424
anas derk <anas.derk@gmail.com>
2025-10-16 15:11:39 +0100

    Fix

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ const nextConfig = {
     removeConsole: false,
   },
   env: {
-    BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3030" : "https://api.crypto-trading.com",
-    WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://crypto-trading.com",
+    BASE_API_URL: process.env.NODE_ENV === "development" ? "http://localhost:3030" : "https://api.proffesorbillionaire.com",
+    WEBSITE_URL: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://proffesorbillionaire.com",
     adminTokenNameInLocalStorage: "s-s-a-t",
     adminDashboardlanguageFieldNameInLocalStorage: "crypto-trading-admin-dashboard-language",
     defaultLanguage: "ar",
@@ -16,12 +16,12 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: process.env.NODE_ENV === "development" ? "//localhost:3030/(.*)" : "//api.crypto-trading.com/(.*)",
+        source: process.env.NODE_ENV === "development" ? "//localhost:3030/(.*)" : "//api.proffesorbillionaire.com/(.*)",
         headers: [
           { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://crypto-trading.com",
+            value: process.env.NODE_ENV === "development" ? "http://localhost:3000" : "https://proffesorbillionaire.com",
           },
           {
             key: "Access-Control-Allow-Methods",
